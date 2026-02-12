e3eca98699f35349a00abeef2ff9b25d0663b01b
boovle2 <PS255115@edu.summacollege.nl>
2026-02-12 13:05:37 +0100

    player movement

--- plataeu/playermovement.gd
+++ plataeu/playermovement.gd
@@ -3,7 +3,9 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+var attack: bool = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,6 +15,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -21,5 +24,28 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	
+	# flip direction
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+	
+	# play animations
+	if(is_on_floor()):
+		if $AnimatedSprite2D.animation != "attack1":
+			if direction == 0:
+				animated_sprite_2d.play("idle")
+			else:
+				animated_sprite_2d.play("run")
+		else:
+			if $AnimatedSprite2D.animation != "attack1":
+				animated_sprite_2d.play("jump")
 	move_and_slide()
+	
+	#attack animation
+	if Input.is_action_pressed("attack1"):
+		print("attack")
+		animated_sprite_2d.play("attack_")
+		
