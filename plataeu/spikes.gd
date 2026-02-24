004b76615088d5838af6e1deb317d12804cf6a8a
Teun Chavanu <PS261726@summacollege.nl>
2026-02-24 21:47:21 +0100

    working spikes

--- plataeu/spikes.gd
+++ plataeu/spikes.gd
@@ -1,19 +1,5 @@
 extends Area2D
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-var move_up = true
-var Speed = 49
-
-func _process(delta: float) -> void:
-	animated_sprite_2d.play("default")
-	if move_up == true:
-		if collision_shape_2d.position.y >= -24 :
-			collision_shape_2d.position.y -= Speed * delta
-		else:
-			move_up = false
-	elif move_up == false:
-		if collision_shape_2d.position.y <= 24:
-			collision_shape_2d.position.y += Speed * delta
-		else:
-			move_up = true
+func _ready() -> void:
+	animation_player.play("default")
